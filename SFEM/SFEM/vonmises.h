c2aeb1f1bc522106fb5e26e82ff1919b52aeb6da
unknown <cecilio.diogo@gmail.com>
2021-03-03 18:38:17 -0300

    Create vonmises.h

---
+++ SFEM/SFEM/vonmises.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "nr3.h"
+#include <math.h>
+#include <cmath>
+#include "elastoplasticbase.h"
+
+
+class vonmises : elastoplasticbase
+{
+public:
+	vonmises(Doub young,Doub nu, Doub sigy);
+	vonmises();
+	~vonmises();
+
+	void closestpointproj(TensorDoub epst, TensorDoub epsp, TensorDoub & projstress, TensorDoub & projstrain, MatDoub & Dep, Doub & projgamma);
+	Doub yield(Doub J2);
+	MatDoub GetElasticMatrix();
+	MatDoub GetInverseElasticMatrix();
+	MatDoub F1HWCylVonMises(Doub xisol, Doub rho, Doub betasol);
+	MatDoub HW(MatDoub sig);
+	MatDoub stressrecosntruction(MatDoub val, MatDoub vec);
+	MatDoub dadsig(TensorDoub sigprojvoigt);
+	MatDoub P();
+	inline void setup(Doub young, Doub nu, Doub sigy) {
+		fyoung = young;
+		fsigy = sigy;
+		fnu = nu;
+		fG = young / (2.* (1. + nu));
+		fK = young / (3.* (1. - 2.* nu));
+	}
+
+private:
+	Doub fyoung;
+	Doub fnu;
+	Doub fsigy;
+	Doub fK;
+	Doub fG;
+};
+
