0927e7891813ec272f008c9ffddbbd69123178ae
unknown <cecilio.diogo@gmail.com>
2021-03-03 18:40:08 -0300

    Create elastoplasticbase.h

---
+++ SFEM/SFEM/elastoplasticbase.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "nr3.h"
+class elastoplasticbase
+{
+public:
+
+	~elastoplasticbase();
+
+	virtual void closestpointproj(TensorDoub epst, TensorDoub epsp, TensorDoub & projstress, TensorDoub & projstrain, MatDoub & Dep, Doub & projgamma)=0;
+	virtual Doub yield(Doub J2)=0;
+	virtual MatDoub GetElasticMatrix()=0;
+	virtual MatDoub GetInverseElasticMatrix()=0;
+	virtual MatDoub F1HWCylVonMises(Doub xisol, Doub rho, Doub betasol)=0;
+	virtual MatDoub HW(MatDoub sig)=0;
+	virtual MatDoub stressrecosntruction(MatDoub val, MatDoub vec)=0;
+	virtual MatDoub dadsig(TensorDoub sigprojvoigt)=0;
+	virtual MatDoub P()=0;
+};
+
