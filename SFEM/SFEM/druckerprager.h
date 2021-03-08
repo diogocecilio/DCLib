4a5922ccc3becce2dc7f7e17beb8c4dc267c8da4
unknown <cecilio.diogo@gmail.com>
2021-03-08 10:38:23 -0300

    Update druckerprager.h
    Dfunc e D2func implemented to use bissection in conjunction whit newton method.

--- SFEM/SFEM/druckerprager.h
+++ SFEM/SFEM/druckerprager.h
@@ -151,3 +151,71 @@ struct Funcstruct:druckerprager {
 };
 
 
+struct Funcd :druckerprager {
+	MatDoub pt;
+	Doub fyoung;
+	Doub fnu;
+	Doub fcoesion;
+	Doub fphi;
+	Doub fK;
+	Doub fG;
+	Doub ftanphi;
+	Doub fapex;
+	Doub fa, fb;
+	Doub operator() (const Doub x) {
+		Doub sig1 = pt[0][0], sig2 = pt[0][1], sig3 = pt[0][2];
+		Doub xi = x;
+		if (xi > fapex) {
+			//xi = fapex;
+		}
+		Doub beta = atan((sqrt(3)*(-sig2 + sig3)) / (-2 * sig1 + sig2 + sig3));
+		Doub G = fG, K = fK, a = fa, b = fb, apex = fapex;
+		return ((-24 * (sqrt(3)*sig1 + sqrt(3)*sig2 + sqrt(3)*sig3 - 3 * xi)) / K + (18 * pow(b, 2)*(-2 * sqrt(3)*apex + 2 * xi)*cos(beta)*
+			(-2 * sig1 + sig2 + sig3 + 2 * sqrt((pow(b, 2)*(-3 * pow(a, 2) + 3 * pow(apex, 2) - 2 * sqrt(3)*apex*xi + pow(xi, 2))) / pow(a, 2))*cos(beta))) /
+			(pow(a, 2)*G*sqrt((pow(b, 2)*(-3 * pow(a, 2) + 3 * pow(apex, 2) - 2 * sqrt(3)*apex*xi + pow(xi, 2))) / pow(a, 2))) +
+			(6 * sqrt(3)*pow(b, 2)*(-2 * sqrt(3)*apex + 2 * xi)*sin(beta)*(-3 * sig2 + 3 * sig3 +
+				2 * sqrt(3)*sqrt((pow(b, 2)*(-3 * pow(a, 2) + 3 * pow(apex, 2) - 2 * sqrt(3)*apex*xi + pow(xi, 2))) / pow(a, 2))*sin(beta))) /
+			(pow(a, 2)*G*sqrt((pow(b, 2)*(-3 * pow(a, 2) + 3 * pow(apex, 2) - 2 * sqrt(3)*apex*xi + pow(xi, 2))) / pow(a, 2)))) / 108.;
+	}
+	Doub df(const Doub x) {
+		Doub sig1 = pt[0][0], sig2 = pt[0][1], sig3 = pt[0][2];
+		Doub xi = x;
+		if (xi > fapex) {
+			//xi = fapex;
+		}
+		Doub beta = atan((sqrt(3)*(-sig2 + sig3)) / (-2 * sig1 + sig2 + sig3));
+		Doub G = fG, K = fK, a = fa, b = fb, apex = fapex;
+		return (72 / K + (72 * pow(b, 2)*pow(-(sqrt(3)*apex) + xi, 2)*pow(cos(beta), 2)) / (pow(a, 2)*G*(-3 * pow(a, 2) + 3 * pow(apex, 2) - 2 * sqrt(3)*apex*xi + pow(xi, 2))) -
+			(9 * pow(b, 4)*pow(2 * sqrt(3)*apex - 2 * xi, 2)*cos(beta)*(-2 * sig1 + sig2 + sig3 + 2 * sqrt((pow(b, 2)*(-3 * pow(a, 2) + 3 * pow(apex, 2) - 2 * sqrt(3)*apex*xi + pow(xi, 2))) / pow(a, 2))*cos(beta))) /
+			(pow(a, 4)*G*pow((pow(b, 2)*(-3 * pow(a, 2) + 3 * pow(apex, 2) - 2 * sqrt(3)*apex*xi + pow(xi, 2))) / pow(a, 2), 1.5)) +
+			(36 * pow(b, 2)*cos(beta)*(-2 * sig1 + sig2 + sig3 + 2 * sqrt((pow(b, 2)*(-3 * pow(a, 2) + 3 * pow(apex, 2) - 2 * sqrt(3)*apex*xi + pow(xi, 2))) / pow(a, 2))*cos(beta))) /
+			(pow(a, 2)*G*sqrt((pow(b, 2)*(-3 * pow(a, 2) + 3 * pow(apex, 2) - 2 * sqrt(3)*apex*xi + pow(xi, 2))) / pow(a, 2))) +
+			(72 * pow(b, 2)*pow(-(sqrt(3)*apex) + xi, 2)*pow(sin(beta), 2)) / (pow(a, 2)*G*(-3 * pow(a, 2) + 3 * pow(apex, 2) - 2 * sqrt(3)*apex*xi + pow(xi, 2))) -
+			(3 * sqrt(3)*pow(b, 4)*pow(2 * sqrt(3)*apex - 2 * xi, 2)*sin(beta)*(-3 * sig2 + 3 * sig3 +
+				2 * sqrt(3)*sqrt((pow(b, 2)*(-3 * pow(a, 2) + 3 * pow(apex, 2) - 2 * sqrt(3)*apex*xi + pow(xi, 2))) / pow(a, 2))*sin(beta))) /
+			(pow(a, 4)*G*pow((pow(b, 2)*(-3 * pow(a, 2) + 3 * pow(apex, 2) - 2 * sqrt(3)*apex*xi + pow(xi, 2))) / pow(a, 2), 1.5)) +
+			(12 * sqrt(3)*pow(b, 2)*sin(beta)*(-3 * sig2 + 3 * sig3 + 2 * sqrt(3)*sqrt((pow(b, 2)*(-3 * pow(a, 2) + 3 * pow(apex, 2) - 2 * sqrt(3)*apex*xi + pow(xi, 2))) / pow(a, 2))*sin(beta))) /
+			(pow(a, 2)*G*sqrt((pow(b, 2)*(-3 * pow(a, 2) + 3 * pow(apex, 2) - 2 * sqrt(3)*apex*xi + pow(xi, 2))) / pow(a, 2)))) / 108.;
+	}
+
+	void setpt(MatDoub ptext, Doub young, Doub nu, Doub coesion, Doub frictionangle)
+	{
+		pt = ptext;
+		setup(young, nu, coesion, frictionangle);
+	}
+	inline void setup(Doub young, Doub nu, Doub coesion, Doub frictionangle) {
+		fyoung = young;
+		fnu = nu;
+		fcoesion = coesion;
+		fphi = frictionangle;
+		fG = young / (2.* (1. + nu));
+		fK = young / (3.* (1. - 2.* nu));
+		ftanphi = (3.*tan(fphi)) / sqrt(9. + 12.* tan(fphi) *tan(fphi));
+		fapex = fcoesion * 1. / tan(fphi);
+		fa = (fcoesion / (sqrt(3)*tan(fphi)) - fapex);
+		fb = fa*ftanphi;
+
+	}
+};
+
+
